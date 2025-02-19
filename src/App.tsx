1c75e66cfc8e4aed2162a8e2f2b9713227ffe4cc
Krister Johansson <krister.johansson86@outlook.com>
2025-02-19 09:34:38 +0100

    chore: add Tailwind CSS and PostCSS configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <h1>Hello World</h1>
+    </div>
+  );
 };
 
 export default App;
