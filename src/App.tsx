dc1675498f468b995080e6eec74afaa1ac5ef28f
Krister Johansson <krister.johansson86@outlook.com>
2025-02-19 09:43:18 +0100

    feat: add shadcn/ui components with Radix UI and Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import WelcomeDialog from "./components/WelcomeDialog";
+
 const App = () => {
   return (
     <div className="h-screen w-full flex items-center justify-center">
-      <h1>Hello World</h1>
+      <WelcomeDialog />
     </div>
   );
 };
---
+++ src/components/WelcomeDialog.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+
+const WelcomeDialog = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default WelcomeDialog;
